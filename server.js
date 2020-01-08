2dbe64c0720b66ca3293977132740d538bf0358c
gtrishali <gtrishali@gmail.com>
2020-01-08 11:15:40 +0530

    yo

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
-cnst path = require('path')
+const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get(express.static(__dirname + '/dist/ngproject'));
+app.use(express.static(__dirname + '/dist/ngproject'));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname + '/dist/ngproject/index.html')))
 
